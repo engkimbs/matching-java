9ec91ee78d8ed22e772159c175fc95b2142242d8
engkimbs <eng.kimbs@gmail.com>
2022-05-10 21:38:56 +0900

    Application class completed

--- src/main/java/engkimbs/matchingjava/Application.java
+++ src/main/java/engkimbs/matchingjava/Application.java
@@ -8,7 +8,6 @@ import org.springframework.context.ApplicationContext;
 
 import java.util.Arrays;
 import java.util.TreeSet;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
@@ -18,19 +17,21 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class Application {
 
-    private Logger log = LoggerFactory.getLogger(Application.class);
+    private final Logger log = LoggerFactory.getLogger(Application.class);
 
     @Value("matching")
     private String appName;
 
     @Bean
-    public void commandLineRunner(ApplicationContext ctx) {
-        String beanNames = Arrays.stream(ctx.getBeanDefinitionNames())
-                .collect(Collectors.toCollection(TreeSet::new))
-                .stream()
-                .collect(Collectors.joining("]\n[", "\n[", "]"));
-        log.info("process=commandLineRunner, status=beansLoaded, beans=$beanNames");
-        log.info("Application \"$appName\" started");
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            String beanNames = Arrays.stream(ctx.getBeanDefinitionNames())
+                    .collect(Collectors.toCollection(TreeSet::new))
+                    .stream()
+                    .collect(Collectors.joining("]\n[", "\n[", "]"));
+            log.info("process=commandLineRunner, status=beansLoaded, beans={}", beanNames);
+            log.info("Application {} started", appName);
+        };
     }
 
     public static void main(String[] args) {
